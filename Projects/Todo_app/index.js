8397d080b5a060070956b85017eadb66eee97f29
VishalRaut-vr <rautv6491@gmail.com>
2026-03-22 12:14:25 +0530

    Refactor todo app to use localStorage for tasks

--- Projects/Todo_app/index.js
+++ Projects/Todo_app/index.js
@@ -1,53 +1,65 @@
-toDoList = [
-    {
-        item:'Buy Milk',
-        dueDate: '29/12/2025',
-    },
-    {
-        item: 'Go out',
-        dueDate: '30/12/2025',
-    }
-];
+let toDoList = JSON.parse(localStorage.getItem("todos")) || [];
+
 displayItems();
 
-function addToDo(){
-    let inputElement = document.querySelector('#Todo_input')
-    let todoItem = inputElement.value;
+function saveData(){
+    localStorage.setItem("todos", JSON.stringify(toDoList));
+}
 
-    let dateElement = document.querySelector('#date_input')
-    let tododate = dateElement.value;
+function addTodo(){
+    let input = document.querySelector('#todo_input');
+    let date = document.querySelector('#date_input');
 
-    toDoList.push({item: todoItem , dueDate: tododate})
+    let item = input.value.trim();
+    let dueDate = date.value;
 
-    inputElement.value = '';
-    dateElement.value = '';
-
-    if (toDoList) {
-        displayItems();
+    if(item === ''){
+        alert("Please enter a task");
+        return;
     }
+
+    toDoList.push({
+        item: item,
+        dueDate: dueDate,
+        completed: false
+    });
+
+    input.value = '';
+    date.value = '';
+
+    saveData();
+    displayItems();
 }
 
 function displayItems(){
-    let containerElements = document.querySelector('.todo_conatainer');
-    let newHtml = '';
+    let container = document.querySelector('#todo_container');
+    let html = '';
+
     for(let i = 0; i < toDoList.length; i++){
-        //let item = toDoList[i].item;
-        //let duedate = toDoList[i].dueDate;
-        let {item,dueDate} = toDoList[i];
-        newHtml = newHtml + `
-        
-        <span>${item}</span>
-        <span>${dueDate}</span>
-        <button  class="btn_delete" onclick="toDoList.splice(${i},1);
-         displayItems();">Delete</button>
+        let {item, dueDate, completed} = toDoList[i];
 
+        html += `
+        <div class="todo_item ${completed ? 'completed' : ''}">
+            <span>${item} (${dueDate})</span>
+            <div>
+                <button class="btn_done" onclick="toggleDone(${i})">✔</button>
+                <button class="btn_delete" onclick="deleteItem(${i})">✖</button>
+            </div>
+        </div>
         `;
-
     }
-    containerElements.innerHTML = newHtml;
+
+    container.innerHTML = html;
 }
-let todoStr = JSON.stringify(toDoList)
-console.log(typeof todoStr)
-console.log(todoStr)
-console.log(typeof toDoList)
-console.log(toDoList)
+
+function deleteItem(index){
+    toDoList.splice(index,1);
+    saveData();
+    displayItems();
+}
+
+function toggleDone(index){
+    toDoList[index].completed = !toDoList[index].completed;
+    saveData();
+    displayItems();
+}
